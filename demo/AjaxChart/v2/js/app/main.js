e88609653fe5b7842e7bf556e31484bb572392d3
danislyn <danislyn@gmail.com>
2015-03-08 10:26:46 +0800

    add JSONP to AjaxChart demo

--- demo/AjaxChart/v2/js/app/main.js
+++ demo/AjaxChart/v2/js/app/main.js
@@ -26,15 +26,40 @@ requirejs(['jquery', 'highcharts', 'AjaxChart'], function($, Highcharts, AjaxCha
     chart1.refresh();
 
 
-    // Chart with local data
-    var chartDataSeries = {"series":[{"name":"Tokyo","data":[["1月",8],["2月",42],["3月",86],["4月",22],["5月",32],["6月",4],["7月",71],["8月",99],["9月",94],["10月",14],["11月",1],["12月",41]]},{"name":"New York","data":[["1月",80],["2月",90],["3月",67],["4月",0],["5月",22],["6月",17],["7月",64],["8月",7],["9月",23],["10月",13],["11月",25],["12月",91]]}]};
-
+    // Chart with JSONP
     var chart2 = new AjaxChart({
         container: '#chartDemo2',
         chartType: 'line'
     });
 
-    chart2.refresh({
+    // jquery写法
+    $.getJSON('http://115.29.242.187:8080/test/testChartJSONP?callback=?', function(result){
+        chart2.refresh({
+            chartData: result
+        });
+    });
+
+    // 原生写法
+    /*var script = document.createElement('script');
+    script.src = 'http://115.29.242.187:8080/test/testChartJSONP?callback=jsonpHandler';
+    document.body.appendChild(script);
+
+    window.jsonpHandler = function(result){
+        chart2.refresh({
+            chartData: result
+        });
+    };*/
+
+
+    // Chart with local data
+    var chartDataSeries = {"series":[{"name":"Tokyo","data":[["1月",8],["2月",42],["3月",86],["4月",22],["5月",32],["6月",4],["7月",71],["8月",99],["9月",94],["10月",14],["11月",1],["12月",41]]},{"name":"New York","data":[["1月",80],["2月",90],["3月",67],["4月",0],["5月",22],["6月",17],["7月",64],["8月",7],["9月",23],["10月",13],["11月",25],["12月",91]]}]};
+
+    var chart3 = new AjaxChart({
+        container: '#chartDemo3',
+        chartType: 'line'
+    });
+
+    chart3.refresh({
         chartData: chartDataSeries
     });
 
@@ -42,12 +67,12 @@ requirejs(['jquery', 'highcharts', 'AjaxChart'], function($, Highcharts, AjaxCha
     // Chart with events
     var chartDataSingle = {"series":[{"name":"New York","data":[["1月",80],["2月",90],["3月",67],["4月",0],["5月",22],["6月",17],["7月",64],["8月",7],["9月",23],["10月",13],["11月",25],["12月",91]]}]};
 
-    var chart3 = new AjaxChart({
-        container: '#chartDemo3',
+    var chart4 = new AjaxChart({
+        container: '#chartDemo4',
         chartType: 'column'
     });
 
-    chart3.refresh({
+    chart4.refresh({
         chartData: chartDataSingle,
         clickHandler: function(xLabel, y){
             alert(xLabel + ': ' + y);
@@ -56,12 +81,12 @@ requirejs(['jquery', 'highcharts', 'AjaxChart'], function($, Highcharts, AjaxCha
 
 
     // Chart with callback
-    var chart4 = new AjaxChart({
-        container: '#chartDemo4',
+    var chart5 = new AjaxChart({
+        container: '#chartDemo5',
         chartType: 'area'
     });
 
-    chart4.refresh({
+    chart5.refresh({
         chartData: chartDataSingle,
         afterUpdate: function(){
             alert('Chart with callback!');
