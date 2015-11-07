9d1c357a0efd3cc99b15f7771bb956e40aa831a3
danislyn <danislyn@gmail.com>
2015-11-07 14:33:10 +0800

    IE搞什么鬼

--- demo/AttackMap/js/config.js
+++ demo/AttackMap/js/config.js
@@ -2,7 +2,7 @@ requirejs.config({
     baseUrl: 'js/lib',
     paths: {
     	jquery: 'http://cdn.staticfile.org/jquery/1.7.2/jquery.min',
-        underscore: 'http://cdn.staticfile.org/underscore.js/1.7.0/underscore-min',
+        underscore: 'http://cdn.staticfile.org/underscore.js/1.7.0/underscore-min'
     },
     packages: [
         {
